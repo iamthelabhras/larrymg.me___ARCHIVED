14f375f699254ff105f7ea4d23ab2884fc7bbc53
Labhrás Mac Gabhann <coding@larrymg.me>
2021-05-18 15:46:01 -0400

    Reorganized larrymg.me according to web dev best practices.  Deploy Sass & Parcel.

--- js/index--old-1.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed('#typed', {
-  strings: ['<span id="hello-gaeilge"><p>Dia dhuit! Labhrás is ainm dom.</p><p>Déanaim suíomhímh gréasáin.</p></span>','<span id="hello-bearla"><p>Hi!  My name is Larry.</p><p>I make websites.</p>'],
-    smartBackspace: true, // Default value;
-    typeSpeed: 50,
-    backSpeed: 20,
-  loop: true,
-  loopCount: 2,
-});
