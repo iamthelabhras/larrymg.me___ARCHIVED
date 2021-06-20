b87fd80d370dd345c975eede6238c6f659af84a4
Labhrás Mac Gabhann <coding@larrymg.me>
2021-06-20 17:04:15 -0400

    Ran npm run build.

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60671" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52791" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
