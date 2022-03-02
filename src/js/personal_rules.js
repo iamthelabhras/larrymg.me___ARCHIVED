5588dc77ddd5fdaa2638e799f12da3d2be7cc592
Labhrás Mac Gabhann <coding@larrymg.me>
2022-03-02 15:16:19 -0500

    refactor: Moved HTML templating logic into modularized JS file.

---
+++ src/js/personal_rules.js
@@ -0,0 +1,21 @@
+// Import relevant modules.
+
+import { allPersonalAssumptions } from "../data/all_personal_assumptions";
+import { allPersonalRules } from "../data/all_personal_rules";
+import { generateCardStack } from "./utility_functions";
+
+// Grab total number of Coding Assumptions & insert into HTML.
+
+const totalPersonalRules = document.querySelector("#total--personal_rules");
+totalPersonalRules.innerHTML = `Personal Rules (${allPersonalRules.length})`;
+
+// Grab total number of Coding Assumptions & insert into HTML.
+
+const totalPersonalAssumptions = document.querySelector("#total--personal_assumptions");
+totalPersonalAssumptions.innerHTML = `Personal Assumptions (${allPersonalAssumptions.length})`;
+
+// Use generateCardStack function to dynamically generate HTML for Coding Rules & Coding Assumptions card stacks.
+
+generateCardStack(allPersonalRules, container_personal_rules);
+generateCardStack(allPersonalAssumptions, container_personal_assumptions);
+
