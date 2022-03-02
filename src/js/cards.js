d2869e6db764e7a2417841f3969daf90486b5d51
Labhrás Mac Gabhann <coding@larrymg.me>
2022-03-02 13:35:39 -0500

    style: Improved code comments.

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,16 +1,20 @@
+// Import relevant modules.
+
 import { allCodingAssumptions } from "../data/all_coding_assumptions";
 import { allCodingRules } from "../data/all_coding_rules";
 import { generateCardStack } from "./utility_functions";
 
+// Grab total number of Coding Assumptions & insert into HTML.
+
 const totalCodingRules = document.querySelector("#total--coding_rules");
 totalCodingRules.innerHTML = `Coding Rules (${allCodingRules.length})`;
 
-// Calculate total number of Coding Assumptions & insert into HTML.
+// Grab total number of Coding Assumptions & insert into HTML.
 
 const totalCodingAssumptions = document.querySelector("#total--coding_assumptions");
 totalCodingAssumptions.innerHTML = `Coding Assumptions (${allCodingAssumptions.length})`;
 
-// Use generateCardStack function to dynamically generate HTML for all card stacks on page.
+// Use generateCardStack function to dynamically generate HTML for Coding Rules & Coding Assumptions card stacks.
 
 generateCardStack(allCodingRules, container_coding_rules);
 generateCardStack(allCodingAssumptions, container_coding_assumptions);
