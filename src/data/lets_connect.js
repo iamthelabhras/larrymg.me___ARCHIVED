ef82a7ef0e8a6d00dae2e93ba8f210118d36a57e
Labhrás Mac Gabhann <coding@larrymg.me>
2022-03-03 11:49:27 -0500

    feat: Added social media and personal networking data.

--- src/data/lets_connect.js
+++ src/data/lets_connect.js
@@ -1,1 +1,14 @@
-export let letsConnect = [];
+export let letsConnect = [
+  {
+    codepen: 'https://codepen.io/iamthelabhras',
+    emailPersonal: 'mailto:hello@larrymg.me',
+    emailWorking: 'mailto:working@larrymg.me',
+    letterboxd: 'https://letterboxd.com/iamthelabhras/',
+    linkedin: 'https://www.linkedin.com/in/larrymg/',
+    resume: 'http://localhost:1234/my_work/resume.html',
+    resumePDF: 'https://labhras.link/213',
+    spotify: 'https://open.spotify.com/user/1287879969?si=feuL7qGwStCY3DLiKp1viw',
+    twitter: 'http://www.twitter.com/iamthelabhras',
+    website: 'https://larrymg.me',
+  }
+];
