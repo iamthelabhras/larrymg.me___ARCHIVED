1cb7949dff10cdc5d1fb7c2f3e2778552a2683fc
Labhrás Mac Gabhann <coding@larrymg.me>
2022-03-03 11:53:34 -0500

    refactor: Moved index.js into js directory.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const font = new FontFace(
-  "cursive_flourish",
-  "url(./fonts/learning_curve_bold_ot_tt.ttf)"
-);
-
-font.load().then(() => {
-  document.fonts.add(font);
-  document.body.style.fontFamily = "cursive_flourish, cursive";
-});
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
-var audio = document.getElementsByTagName("audio")[0];
-audio.play()
 
-var audio = $("#mySoundClip")[0];
+// TO DO: Figure out why I put this in here?
 
-$("nav a").mouseenter(function() {
-  audio.play();
+const font = new FontFace(
+  "cursive_flourish",
+  "url(./fonts/learning_curve_bold_ot_tt.ttf)"
+);
+
+font.load().then(() => {
+  document.fonts.add(font);
+  document.body.style.fontFamily = "cursive_flourish, cursive";
 });
