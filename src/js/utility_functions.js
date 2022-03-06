a96a26116f161b8d6852a156dc3f4a7f26027603
Labhrás Mac Gabhann <coding@larrymg.me>
2022-03-06 03:28:39 -0500

    feat: Created generate functions for all This Week sidebar items.

--- src/js/utility_functions.js
+++ src/js/utility_functions.js
@@ -1,3 +1,9 @@
+import { thisWeekLearning } from "../data/this_week";
+import { thisWeekReading } from "../data/this_week";
+import { thisWeekWatching } from "../data/this_week";
+import { thisWeekHotTrack } from "../data/this_week";
+import { thisWeekHotLaugh } from "../data/this_week";
+
 export function generateCardStack(arrayOfCards, cardContainerSelector) {
   // Initialize a variable to collect the HTML for our entire card stack. 
   let html ="";
@@ -90,4 +96,122 @@ export function generateCardStack(arrayOfCards, cardContainerSelector) {
   console.log(html);
   cardContainerSelector.innerHTML = `${html}</div>`;
 
+};
+
+export function generateThisWeekLearning() {
+  let learningList = document.querySelector("#what_im_learning_list");
+  thisWeekLearning.subjects.forEach(function (subject) {
+    let html = "";
+    if (subject.subjectBullets !== []) {
+      html += `<li>`; 
+      html += `${subject.subjectName}`;
+      html += `<ul>`;
+      subject.subjectBullets.forEach(bullet => html += `<li>${bullet}</li>`);
+      html += `</ul>`;
+      html += `</li>`;
+      }
+    else {
+      html += `<li>`;
+      html += `${subject.subjectName}`;
+      html += `<li>`;
+      }
+    learningList.innerHTML += html;
+    });
+};
+
+export function generateThisWeekReading() {
+  let readingList = document.querySelector("#what_im_reading_list");
+  thisWeekReading.readings.forEach(function (reading) {
+    let html = "";
+    if (reading.readingAuthorLink !== "") {
+      html += `<li>`;
+      html += `${reading.readingTitle}`;
+      html += `<span class="author">`;
+      html += `<a href="`;
+      html+= `${reading.readingAuthorLink}`;
+      html += `" target="_blank">`;
+      html += `${reading.readingAuthor}`;
+      html += `</a>`;
+      html += `</li>`;
+      }
+    else {
+      html += `<li>`; 
+      html += `${reading.readingTitle}`;
+      html += `<span class="author">`;
+      html += `${reading.readingAuthor}`;
+      html += `</li>`;
+      }
+    readingList.innerHTML += html;
+    });
+};
+
+export function generateThisWeekWatching() {
+  let watchingList = document.querySelector("#what_im_watching_list");
+  thisWeekWatching.videos.forEach(function (video) {
+    let html = "";
+    html += `<li>`
+    if (video.videoTrailer !== "") {
+      html += `<a href="`;
+      html += `${video.videoTrailer}`;
+      html += `" target="_blank">`;
+      html+= `${video.videoTitle}`;
+      html += `</a>`;
+      }
+    else {
+      html += `${video.videoTitle}`;
+      }
+    html += `<span class="clean_break_on_wrap">`;
+    html += ` (${video.videoYear}, `;
+    if (video.videoDirectorLink !== "") {
+      html += `dir. `;
+      html += `<a href="`
+      html += `${video.videoDirectorLink}`;
+      html += `" target="_blank">`
+      html += `${video.videoDirector}`;
+      html += `</a>)`;
+    }
+    else {
+      html += `dir. `;
+      html += `${video.videoDirector})`;
+    };
+    html += `</span>`;
+    html += `</li>`;
+    watchingList.innerHTML += html;
+    });
+};
+
+export function generateThisWeekHotTrack () {
+    let hotTrack = document.querySelector("#hot_track_container");
+    let html = "";
+    html += `<iframe width="560" height="315"`;
+    html += `src="${thisWeekHotTrack.iframeSrc}"`;
+    html += `title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>`
+    html += `</iframe>`
+    html += `<span class="text_center">`
+    html += `<a href="${thisWeekHotTrack.trackArtistLink}" target="_blank">`;
+    html += `${thisWeekHotTrack.trackArtist}'s</a> `;
+    html += `<span class="clean_break_on_wrap">`
+    html += `<a href="${thisWeekHotTrack.trackTitleLink}" target="_blank">`;
+    html += `"${thisWeekHotTrack.trackTitle}"`;
+    html += `</a>`;
+    html += `</span>`;
+    hotTrack.innerHTML += html;
+};
+
+export function generateThisWeekHotLaugh () {
+  let hotLaugh = document.querySelector("#hot_laugh_container");
+  let html = "";
+  html += `<iframe width="560" height="315"`;
+  html += `src="${thisWeekHotLaugh.iframeSrc}"`;
+  html += `title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>`
+  html += `</iframe>`
+  html += `<span class="text_center">`
+  html += `<a href="${thisWeekHotLaugh.laughArtistLink}" target="_blank">`;
+  html += `${thisWeekHotLaugh.laughArtist}'s</a> `;
+  html += `<span class="clean_break_on_wrap">`
+  html += `<a href="${thisWeekHotLaugh.laughTitleLink}" target="_blank">`;
+  html += `${thisWeekHotLaugh.laughTitle}`;
+  html += `</a>`;
+  html += `</span>`;
+  hotLaugh.innerHTML += html;
 };
