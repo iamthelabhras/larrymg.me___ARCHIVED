e01ac333653793e575f104a748ca67d0651f4fab
Labhrás Mac Gabhann <coding@larrymg.me>
2022-03-06 03:27:56 -0500

    Moved This Week sidebar data into standalone exportable modules.

---
+++ src/data/this_week.js
@@ -0,0 +1,49 @@
+export let thisWeekLearning = {
+  subjects: [
+    {
+      subjectName: "JavaScript Fundamentals",
+      subjectBullets: ["This week's deep focus 👀: objects & arrays."],
+    }, 
+  ],
+};
+
+export let thisWeekReading = {
+  readings: [
+    {
+      readingTitle: "JavaScript: The Definitive Guide",
+      readingAuthor: "David Flanagan",
+      readingAuthorLink: "https://twitter.com/__DavidFlanagan"
+    }
+  ],
+};
+
+
+export let thisWeekWatching = {
+    videos: [
+    { 
+      videoTitle: "Le Samourai",
+      videoTrailer: "https://www.youtube.com/watch?v=Fs0XYssIlbo",
+      videoYear: "1967",
+      videoDirector: "Melville",
+      videoDirectorLink: "https://en.wikipedia.org/wiki/Jean-Pierre_Melville",
+    },
+  ],
+};
+
+export let thisWeekHotTrack = 
+  {
+    trackTitle: "This Is My Beloved",
+    trackTitleLink: "https://www.youtube.com/watch?v=MVTbRt7jWPg",
+    trackArtist: "Mort Garson",
+    trackArtistLink: "https://en.wikipedia.org/wiki/Mort_Garson", 
+    iframeSrc: "https://www.youtube.com/embed/MVTbRt7jWPg",
+  };
+
+export let thisWeekHotLaugh =
+  {
+    laughTitle: "'Bug Juice' Sketch",
+    laughTitleLink: "https://www.youtube.com/watch?v=xA6EXvyWlpY",
+    laughArtist: "Upright Citizens Brigade",
+    laughtArtistLink: "https://en.wikipedia.org/wiki/Upright_Citizens_Brigade", 
+    iframeSrc: "https://www.youtube.com/embed/xA6EXvyWlpY",
+  };
