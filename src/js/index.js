7b200855cb97debb41ea0f4d6fb9c6e1c8533ce4
Labhrás Mac Gabhann <coding@larrymg.me>
2022-03-09 15:31:13 -0500

    feat: Imported personal data.

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,8 @@ import { generateThisWeekWatching } from "../js/utility_functions.js";
 import { generateThisWeekHotTrack} from "../js/utility_functions.js";
 import { generateThisWeekHotLaugh} from "../js/utility_functions.js";
 
+import { personal_data } from "../data/personal.js";
+
 // Let's Connect
 
 codepen.setAttribute("href", `${letsConnect.codepen}`);
@@ -23,3 +25,9 @@ generateThisWeekReading();
 generateThisWeekWatching();
 generateThisWeekHotTrack();
 generateThisWeekHotLaugh();
+
+// Current Location
+
+currentLocation = document.querySelector("#current_location");
+console.log(currentLocation);
+currentLocation.textContent = `${personal_data.current_location}`;
