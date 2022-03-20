9b729aeb764aa02248292e35dd8b42ec8ad4aacd
Labhrás Mac Gabhann <coding@larrymg.me>
2022-03-19 21:17:25 -0400

    fix: Updated object properties on rules & assumptions to align with refactored generateCardStack().

--- src/data/all_coding_assumptions.js
+++ src/data/all_coding_assumptions.js
@@ -2,72 +2,72 @@ export let allCodingAssumptions = [
 
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
-    social_link: "",
-    source_link: "",
+    social_link: undefined,
+    source_link: undefined,
     source: "A. Bell",
     body: "We build the web for everyone, not ourselves or our peers.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
   
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/billsourour",
-    source_link: "",
+    source_link: undefined,
     source: "B. Sourour",
     body: "Almost every bug rests on a bad assumption.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
   
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/sarah_edo",
-    source_link: "",
+    source_link: undefined,
     source: "S. Drasner",
     body: "By choosing to be a developer, you are choosing to learn.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
   
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
-    social_link: "",
-    source_link: "",
+    social_link: undefined,
+    source_link: undefined,
     source: "Anonymous",
     body: "It only takes on instance of losing a few hours of work to know that slow and steady is the way to go.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   }, 
 
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/realdonaldknuth",
-    source_link: "",
+    source_link: undefined,
     source: "D. Knuth",
     body: "People that comptuer science is the art of geniuses but the actual reality is the opposite: just many people doing things that build on each, like a wall of mini-stones.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
   
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/syntaxfm",
-    source_link: "",
+    source_link: undefined,
     source: "Syntax Podcast",
     body: "If you write it, you don't have to learn it.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   }, 
 
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
-    social_link: "",
-    source_link: "",
+    social_link: undefined,
+    source_link: undefined,
     source: "Anonymous",
     body: "The first 90% of your code accounts for the first 90% of development time; the remaining 10% of the code accounts for the other 90% of development time.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
   
   {
@@ -76,54 +76,54 @@ export let allCodingAssumptions = [
     source_link: "https://zerodivs.com/#/about",
     source: "J. Perals",
     body: "The first 90% of your code accounts for the first 90% of development time; the remaining 10% of the code accounts for the other 90% of development time.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   }, 
   
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/jamesqquick",
-    source_link: "",
+    source_link: undefined,
     source: "J. Quick",
     body: "The best way to learn is to build.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
   
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/KevinJPowell",
-    source_link: "",
+    source_link: undefined,
     source: "K. Powell",
     body: "CSS is a visual language at the end of the day: make the visual punch you in the face so you really understand what's going on, then tone things down.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   }, 
 
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/KevinJPowell",
-    source_link: "",
+    source_link: undefined,
     source: "K. Powell",
     body: "Although we use several different languages when putting together websites, CSS is the only one that has to deal with the 'webiness' of the web.",
-    justifications: "",
+    justifications: undefined,
     concrete_applications: ["This is the primary challenge of working with CSS!"],
   },
   
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/holtbt",
-    source_link: "",
+    source_link: undefined,
     source: "B. Holt",
     body: "If something is delete-able, it's modular; and if it's modular, it's great ",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
   
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/billsourour",
-    source_link: "",
+    source_link: undefined,
     source: "B. Sourour",
     body: "The best devs deeply understand how the languages & frameworks they use actually work (versus how they think they work).",
     justifications: ["Almost every bug rests on a bad assumption."],
@@ -133,31 +133,31 @@ export let allCodingAssumptions = [
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/rachelandrew",
-    source_link: "",
+    source_link: undefined,
     source: "R. Andrews",
     body: "Mastering core skills & concepts (e.g. HTML, Pure CSS, Vanilla JS) allows devs to hold the tools built on top of those skills lightly, learn quicky, and switch easily.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
   
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/MarijnJH",
-    source_link: "",
+    source_link: undefined,
     source: "M. Haverbeke",
     body: "Computing new values is less error-prone than changing existing values.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   }, 
 
   {
     card_number: undefined,  // value is dynamically via .forEach method below.
     social_link: "https://twitter.com/goldbergyoni",
-    source_link: "",
+    source_link: undefined,
     source: "Y. Goldberg",
     body: "A test should feel like a friendly assistant - there to help you - and should never feel like a hindrance.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   }, 
 
 ];
--- src/data/all_coding_rules.js
+++ src/data/all_coding_rules.js
@@ -3,7 +3,7 @@ export let allCodingRules = [
   {
     card_number: undefined,  // this value is dynamically generated via .forEach method below.
     social_link: "https://twitter.com/billsourour",
-    source_link: "",
+    source_link: undefined,
     source: "B. Sourour",
     body: "Refactor code into lots of small, independent components whenever & wherever you can.",
     justifications: ["This makes code easier to reason about!", "Not following this rule makes code difficult to reuse & costly to change."],
@@ -16,58 +16,58 @@ export let allCodingRules = [
     source_link: "https://css-tricks.com/how-well-do-you-know-css-layout/",
     source: "B. Westfall",
     body: "Don't change a UI to accomodate Box Model issues that are easily avoided.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
 
   {
     card_number: undefined,  // this value is dynamically generated via .forEach method below.
     social_link: "https://twitter.com/bradwestfall",
-    source_link: "",
+    source_link: undefined,
     source: "B. Westfall",
     body: "Always look for patterns when solving problems.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
 
   {
     card_number: undefined,  // this value is dynamically generated via .forEach method below.
     social_link: "https://twitter.com/billsourour",
-    source_link: "",
+    source_link: undefined,
     source: "B. Sourour",
     body: "First make it work; then make it better.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
 
   {
     card_number: undefined,  // this value is dynamically generated via .forEach method below.
     social_link: "https://twitter.com/holtbt",
-    source_link: "",
+    source_link: undefined,
     source: "B. Holt",
     body: "Optimize for delete-ability.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
 
   {
     card_number: undefined,  // this value is dynamically generated via .forEach method below.
     social_link: "https://twitter.com/martinfowler",
-    source_link: "",
+    source_link: undefined,
     source: "M. Fowler",
     body: "Commit frequently & only do one thing per commit.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
 
   {
     card_number: undefined,  // this value is dynamically generated via .forEach method below.
     social_link: "https://twitter.com/iamthelabhras",
-    source_link: "",
+    source_link: undefined,
     source: "Mise Mé Féin",
     body: "When starting a new side project, create the site locally first and then purchase the domain name.",
     justifications: ["At any give time, Gandi has too many cool domain names on sale."],
-    concrete_applications: "",
+    concrete_applications: undefined,
   },
 
   {
@@ -76,8 +76,8 @@ export let allCodingRules = [
     source_link: "https://www.smashingmagazine.com/2016/05/fluid-typography/",
     source: "M. Riethmuller",
     body: "To avoid older browser bugs with calc() & viewport units (e.g. vw & vh ), set sensible default font sizes before declaring a fluid calc() expression.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
 
   {
@@ -87,7 +87,7 @@ export let allCodingRules = [
     source: "C. Schafer",
     body: "GUI tools are useful but it's better to learn command line tools from the beginning.",
     justifications: ["GUIs can only do so much and you will eventually run into a situation where you get stuck and the underlying knowledge of the command line would be extremely useful."],
-    concrete_applications: "",
+    concrete_applications: undefined,
   },
 
   {
@@ -96,8 +96,8 @@ export let allCodingRules = [
     source_link: "https://github.com/goldbergyoni/javascript-testing-best-practices",
     source: "Y. Goldberg",
     body: "One should look at a test and get the intent instantly.",
-    justifications: "",
-    concrete_applications: "",
+    justifications: undefined,
+    concrete_applications: undefined,
   },
 
 ];
