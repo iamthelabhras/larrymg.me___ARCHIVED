368bf2b0c6a42fa156a0f150f366d58e06998c95
Labhrás Mac Gabhann <coding@larrymg.me>
2022-03-19 22:44:13 -0400

    style: Fixed typos & missing characters.

--- src/data/all_commonplace_book_entries.js
+++ src/data/all_commonplace_book_entries.js
@@ -4,7 +4,7 @@ export let allCommonplaceBookEntries = [
     author_link: 'https://twitter.com/NateSilver538',
     body: 'It probably doesn\'t end well, but it isn\'t clear yet in which way it ends poorly.',
     platform: 'Social Media',
-    source: 'Tweet',
+    source: 'Twitter',
     source_date: '10/5/2020',
     source_link: 'https://twitter.com/NateSilver538/status/1313294739731623936?s=20&t=bqjR5ZrMiR5P5adiECLlLw'
   },
@@ -13,16 +13,16 @@ export let allCommonplaceBookEntries = [
     author_link: 'https://en.wikipedia.org/wiki/Vladimir_Nabokov',
     body: 'Life is a great surprise. I do not see why death should not be an even greater one.',
     platform: 'Novel',
-    source: 'Pale Fire',
+    source: '<i>Pale Fire</i>',
     source_date: '1962',
     source_link: 'https://libgen.is/book/index.php?md5=BB96357BCDB1ED90860C4D93C2834B9A' 
   },
   {
     author: 'Donald Hall',
     author_link: 'https://en.wikipedia.org/wiki/Donald_Hall',
-    body: 'Now and then, especially at night, solitude loses its soft power and loneliness takes over. I am grateful when solitude returns',
+    body: 'Now and then, especially at night, solitude loses its soft power and loneliness takes over. I am grateful when solitude returns.',
     platform: 'Essay',
-    source: 'Between Solitude and Loneliness',
+    source: '"Between Solitude and Loneliness"',
     source_date: '10/15/2016',
     source_link: 'https://www.newyorker.com/culture/culture-desk/double-solitude',
   },
@@ -31,7 +31,7 @@ export let allCommonplaceBookEntries = [
     author_link: 'https://en.wikipedia.org/wiki/Paul_Schrader',
     body: 'My father often talked of things being revealed - that was true invention, he said.  Revealing something\'s use, and magnifying it; discovering its imperfections, improving it, and putting it to work for you.  God had left the world incomplete, he said, and it was man\'s job to understand how it worked, tinker with it, and to finish it.  I think that was why he hated missionaries so much - because they taught people to put up with their earthly burdens.  For father, there were no burdens that couldn\'t be fitted with a set of wheels, or rudders, or a system of pulleys.',
     platform: 'Film',
-    source: 'Mosquito Coast',
+    source: '<i>Mosquito Coast</i>',
     source_date: '1986',
     source_link : 'https://www.imdb.com/title/tt0091557/',
   },
@@ -40,7 +40,7 @@ export let allCommonplaceBookEntries = [
     author_link: 'https://en.wikipedia.org/wiki/C._L._R._James',
     body: 'Great men make history but only such history as it is possible for them to make. Their freedom of achievement is limited by the necessities of their environment. To portray the limits of those necessities and the realisation, complete or partial, of all possibilities, that is the true business of the historian.',
     platform: 'Essay',
-    source: 'The Black Jacobins',
+    source: '<i>The Black Jacobins</i>',
     source_date: '1938',
     source_link: 'https://libgen.is/book/index.php?md5=E4AFE8BB5BC88E50516CF0C01290342C'
 
@@ -51,7 +51,7 @@ export let allCommonplaceBookEntries = [
     author_link: 'https://en.wikipedia.org/wiki/Jessa_Crispin',
     body: 'I understand that this was my female relatives\' choice. Yet I also know how infrequently you truly make the choice to sacrifice something you love; it\'s more like a slow erosion. You give up one small thing, which makes it easier then to give up another, then another. After all, the men didn\'t ask them to make these sacrifices — asking that would require their acknowledgment there was something of value to give up.',
     platform: 'Essay',
-    source: 'St Teresa and the Single Ladies',
+    source: '"St. Teresa and the Single Ladies"',
     source_date: '2016',
     source_link: 'https://www.nytimes.com/2016/01/10/opinion/sunday/st-teresa-and-the-single-ladies.html' 
   },
